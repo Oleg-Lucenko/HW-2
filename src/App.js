d691df9e317e43c37d1c248840fbea133c5b2c5d
Oleg-Lucenko <lucenko5577@gmail.com>
2023-07-14 19:07:01 +0300

    HW-2, Lucenko

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import {useState} from 'react';
+
 import Counter from './components/Counter';
 
 function App() {
 
-  const [counter, changeCounter] = useState(0);
 
   return (
     <div className="App">
